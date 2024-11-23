52d8f2c81012aedb6f9fdd0f54c4d71120bc4769
Konstantin Gorodetskiy <kpgorodetskiy@sberautotech.ru>
2024-11-23 10:13:34 +0300

    Добавил минимальную структуру бота

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 """Init wildberries telegram bot."""
 
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+import creds
+from handlers import router
+
+
+async def main():
+    bot = Bot(token=creds.TELE_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
 
 if __name__ == "__main__":
-    pass
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
